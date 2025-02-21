4c5f10f66876f6d43b0b42c804354cde366c5327
sagorsheelrahul <sagorsheelrahul@gmail.com>
2025-02-21 07:54:58 +0000

    Added CSV file and download scripts

--- download_config.py
+++ download_config.py
@@ -1,15 +1,15 @@
 class Config:
     def __init__(self):
-        # Path to your uploaded CSV file (already in your repo)
+        # Path to your uploaded CSV file
         self.input_path = 'NCBI_22June_RefSeq_32927_Complete_1NP_2P_taxnonmy.csv'
         # Directory to save the downloaded FASTA files
         self.output_path = 'FASTA_files/'
-        # Number of CPU workers (use fewer for Colab, as too many can cause issues)
+        # Number of CPU workers
         self.cpu_worker_num = 4
         # File suffix for the genomic FASTA files
         self.file_type = '_genomic.fna.gz'
-        # Column name in your CSV that contains URLs
-        self.csv_col = 'urls'
+        # ✅ Correct FTP column name
+        self.csv_col = 'ftp_path'  # <-- Updated here
         # Number of retry attempts for each download
         self.retry_times = 10
         # Path to log file for any download failures
